94d61d375af407e3484da835508b3280a3ab7ccb
Mervin <mengqing723@gmail.com>
2025-01-05 19:07:19 +0800

    perf(random): Optimizing Random Generation Functions

--- core/src/random/random.ts
+++ core/src/random/random.ts
@@ -1,8 +1,12 @@
 /**
- * 随机 8位id
+ * 随机8位id
  */
 export const randomId = () => Math.floor(Math.random() * Date.now()).toString(36)
 
+/**
+ * 生成 uuid
+ * @returns
+ */
 export function uuid() {
 	let d = Date.now()
 	if (typeof window !== 'undefined' && window.performance) {
@@ -21,18 +25,40 @@ export function uuid() {
  * const uuid = () => URL.createObjectURL(new Blob()).split('/').pop()
  */
 
+/**
+ * 生成随机整数
+ * @param min 最小值
+ * @param max 最大值
+ * @returns
+ */
 export function randomInt(min: number, max: number) {
 	return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+/**
+ * 生成随机浮点数
+ * @param min 最小值
+ * @param max 最大值
+ * @returns
+ */
 export function randomFloat(min: number, max: number) {
 	return Math.random() * (max - min) + min
 }
 
+/**
+ * 生成随机布尔值
+ * @returns
+ */
 export function randomBool() {
 	return Math.random() > 0.5
 }
 
+/**
+ * 生成随机字符串
+ * @param length 长度
+ * @param characters 字符集
+ * @returns
+ */
 export function randomString(length: number, characters?: string) {
 	let result = ''
 	if (!characters) {
@@ -46,7 +72,7 @@ export function randomString(length: number, characters?: string) {
 }
 
 /**
- * 带权重的随机选择
+ * 生成随机元素, 支持设置权重
  * @param items 元素列表
  * @param weights 权重列表
  * @returns
@@ -61,7 +87,7 @@ export function randomItem<T>(items: T[], weights?: number[]): T {
 		return items[Math.floor(Math.random() * items.length)]
 	}
 
-	// 计算累计权重 [1,1,3] = [1,2,5]
+	// 计算累计权重 [2,2,5] = [2,4,9]
 	const cumulate = weights.map(
 		(
 			(sum) => (value) =>
@@ -80,6 +106,11 @@ export function randomItem<T>(items: T[], weights?: number[]): T {
 	return items[items.length - 1]
 }
 
+/**
+ * 随机打乱数组
+ * @param array 数组
+ * @returns
+ */
 export function shuffleArray(array: any[]) {
 	return array
 		.map((value) => ({ value, sort: Math.random() }))
@@ -87,12 +118,22 @@ export function shuffleArray(array: any[]) {
 		.map(({ value }) => value)
 }
 
+/**
+ * 生成随机颜色
+ * @returns
+ */
 export function randomColor() {
 	return `#${Math.floor(Math.random() * 0xffffff)
 		.toString(16)
 		.padStart(6, '0')}`
 }
 
+/**
+ * 生成随机日期
+ * @param start 开始日期
+ * @param end 结束日期
+ * @returns
+ */
 export function randomDate(start: Date | string | number, end: Date | string | number): Date {
 	const startTime = new Date(start).getTime()
 	const endTime = new Date(end).getTime()
