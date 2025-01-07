10df36ac282075fbf1e950edd4a33b4ea03a73f9
Mervin <mengqing723@gmail.com>
2025-01-07 09:29:03 +0800

    perf: Refactor isType function with overloads

--- core/src/type/isType.ts
+++ core/src/type/isType.ts
@@ -1,7 +1,11 @@
-export default function isType(value: any, type?: string): string | boolean {
+function isType(value: any): string
+function isType(value: any, type: string): boolean
+function isType(value: any, type?: string): string | boolean {
 	const valType = Object.prototype.toString.call(value).slice(8, -1).toLowerCase()
 	if (type && typeof type === 'string') {
 		return valType === type.toLowerCase()
 	}
 	return valType
 }
+
+export default isType
